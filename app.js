414992fd8bdeb7046f0598508055dd46f0f7c389
Claudio Petrini <claudio.petrini@stamplay.com>
2014-07-24 21:55:27 +0200

    Basic server functionalities

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
+
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/', routes);
+
+app.use('/ok', function (req, res, next) {
+    res.send(200);
+});
+
+app.use('/ko', function (req, res, next) {
+    res.send(500);
+});
+
+module.exports = app;
